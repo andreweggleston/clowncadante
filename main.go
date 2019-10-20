7fd6eb4970ef5234db870f691259dd1a7fce7993
Andrew Eggleston <egglestonandrew927@gmail.com>
2019-10-20 17:12:28 -0400

    clown time

--- main.go
+++ main.go
@@ -43,6 +43,7 @@ func main() {
 			innerEvent := eventsAPIEvent.InnerEvent
 			switch ev := innerEvent.Data.(type) {
 			case *slackevents.MessageEvent:
+				fmt.Println(ev.Message)
 				if ev.User == os.Getenv("MERKY_UID"){
 					r := rand.New(rand.NewSource(200))
 					if r.Intn(100) > 80 {
